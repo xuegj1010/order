8a7eb2f51555b6c21a25ef770a4457365ad74299
xuegj1010 <869624090@qq.com>
2019-03-04 22:02:47 +0800

    添加ueditor编辑器

---
+++ web/static/js/food/set.js
@@ -0,0 +1,24 @@
+;
+var food_set_ops = {
+    init: function () {
+        this.eventBind();
+        this.initEditor();
+    },
+    eventBind: function () {
+
+    },
+    initEditor: function () {
+        var that = this;
+        that.ue = UE.getEditor('editor', {
+            enableAutoSave: true,
+            saveInterval: 60000,
+            elementPathEnabled: false,
+            zIndex: 4,
+            erverUrl: common_ops.buildUrl('/upload/ueditor')
+        })
+    }
+};
+
+$(document).ready(function () {
+    food_set_ops.init();
+});
